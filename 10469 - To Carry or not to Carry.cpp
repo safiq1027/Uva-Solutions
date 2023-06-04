88eb0ee7d6c244043fcf1329f6483bf5006fb4f5
Md Safiqul Islam <112417622+safiq1027@users.noreply.github.com>
2023-06-04 16:33:29 +0600

    Easy .X-OR (AB^bar +A^barB)

---
+++ 10469 - To Carry or not to Carry.cpp
@@ -0,0 +1,12 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main()
+ {
+     int x,y;
+     while(cin>>x>>y){
+        int res=x^y;
+        cout<<res<<endl;
+     }
+     return 0;
+ }
+
