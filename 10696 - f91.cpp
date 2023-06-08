0d749ae5f9b4667ce148221323d36bc0e2d2131e
Md Safiqul Islam <112417622+safiq1027@users.noreply.github.com>
2023-06-08 11:45:57 +0600

    Add files via upload

---
+++ 10696 - f91.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n&&n!=0)
+    {
+        if(n>=101){
+            cout<<"f91("<<n<<") = "<<n-10<<endl;
+        }
+        else
+        {
+           cout<<"f91("<<n<<") = "<<"91"<<endl;
+        }
+    }
+
+    return 0;
+}
+
